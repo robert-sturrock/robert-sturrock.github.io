701864e8199d8d39146c6b1e6a2cb887bf4dafd9
Robert <robert@sturrock.ca>
2018-05-13 22:03:51 +0100

    updating the website

---
+++ js/clean-blog-test.js
@@ -0,0 +1,70 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Floating label headings for the contact form
+  $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+    $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+  }).on("focus", ".floating-label-form-group", function() {
+    $(this).addClass("floating-label-form-group-with-focus");
+  }).on("blur", ".floating-label-form-group", function() {
+    $(this).removeClass("floating-label-form-group-with-focus");
+  });
+
+  // Show the navbar when the page is scrolled up
+  var MQL = 992;
+
+  //primary navigation slide-in effect
+  if ($(window).width() > MQL) {
+    var headerHeight = $('#mainNav').height();
+    $(window).on('scroll', {
+        previousTop: 0
+      },
+      function() {
+        var currentTop = $(window).scrollTop();
+        //check if user is scrolling up
+        if (currentTop < this.previousTop) {
+          //if scrolling up...
+          if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
+            $('#mainNav').addClass('is-visible');
+          } else {
+            $('#mainNav').removeClass('is-visible is-fixed');
+          }
+        } else if (currentTop > this.previousTop) {
+          //if scrolling down...
+          $('#mainNav').removeClass('is-visible');
+          if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
+        }
+        this.previousTop = currentTop;
+      });
+  }
+
+})(jQuery); // End of use strict
+
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      } 
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
